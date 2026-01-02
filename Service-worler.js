caf0d73c6a06390926195e909b3451ba548c66e1
hichemouledomar-droid <hichem.ouledomar@worldlearningalgeria.org>
2026-01-02 23:19:24 +0100

    Add simple service worker for caching

---
+++ Service-worler.js
@@ -0,0 +1,21 @@
+// Simple Service Worker for caching
+const CACHE_NAME = 'dawaa-v1';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
